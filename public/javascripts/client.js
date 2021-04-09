6ca881be6a0a679383ac7dae72112f108a4c7225
Tyler Le <tylerle99@yahoo.com>
2021-04-09 15:05:38 -0700

    delete item functionality for shopping cart

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,18 +1,26 @@
-document.getElementById("low-to-high")
-  .addEventListener("click", async () => {
-    console.log("HELLO")
-    // Manipulate DOM here
-    // How to access MongoDB?
-  });
+// document.getElementById("low-to-high")
+//   .addEventListener("click", async () => {
+//     console.log("HELLO")
+//     // Manipulate DOM here
+//     // How to access MongoDB?
+//   });
 
-document.getElementsByClassName("cart-remove")
-  .addEventListener("click", async () => {
-    console.log("CART REMOVE")
-    // Manipulate DOM here
-    // How to access MongoDB?
-  });
+// document.getElementsByClassName("cart-remove")
+//   .addEventListener("click", async () => {
+//     console.log("CART REMOVE")
+//     // Manipulate DOM here
+//     // How to access MongoDB?
+//   });
 
-  document.querySelector("cart-remove")
-  .addEventListener("click", async () => {
-    console.log("CART REMOVE")
+// Very hacky, try to refactor
+const cart_remove = document.getElementsByClassName("cart-remove");
+for (let i = 0; i < cart_remove.length; i++) {
+  cart_remove[i].addEventListener("click", () => {
+    try {
+      cart_remove[i].parentElement.remove();
+    } catch (error) {
+      i=0;
+      cart_remove[i].parentElement.remove();
+    }
   });
+}
