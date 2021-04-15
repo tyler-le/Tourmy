5d3d89143041cd4f834b4fd47cb64869d0777ce8
Tyler Le <tylerle99@yahoo.com>
2021-04-15 15:30:35 -0700

    cart summary margin

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -6,26 +6,26 @@ for (let i = 0; i < cart_remove.length; i++) {
     parent = cart_remove[i].parentElement;
     console.log(parent)
     parent.style.display = 'none';
+    
   });
 }
 
 // +/- item functionality
 const qty = document.querySelectorAll("input");
 
+const plus = document.getElementsByClassName("fa-plus");
+for (let i = 0; i < plus.length; i++) {
+  plus[i].addEventListener("click", () => {
+    qty[i].value++;
+    // cart_price_span[i].innerText = `$${item_prices[1]}.00`;
+  })
+}
 const minus = document.getElementsByClassName("fa-minus");
 for (let i = 0; i < minus.length; i++) {
   minus[i].addEventListener("click", () => {
     if (qty[i].value > 0) {
       qty[i].value--;
     }
-  })
-}
-
-const plus = document.getElementsByClassName("fa-plus");
-for (let i = 0; i < plus.length; i++) {
-  plus[i].addEventListener("click", () => {
-    qty[i].value++;
-    // cart_price_span[i].innerText = `$${item_prices[1]}.00`;
   })
 }
 
@@ -47,3 +47,4 @@ for (let i = 0; i < item_prices.length; i++) {
 subtotal_span[0].innerText = `$${subtotal}.00`
 
 // cart_price_span[0].innerText = `$${item_prices[1]}.00`;
+
