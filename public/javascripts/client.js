fe0165d1c433aaf5e04d2dd7ed4260edecca24ea
Tyler Le <tylerle99@yahoo.com>
2021-04-19 16:04:00 -0700

    refactored code and added functionality to all buttons

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -2,15 +2,22 @@ const initializeCart = () => {
   for (let i = 0; i < cart_price_span.length; i++) {
     item_prices[i] = cart_price_span[i].innerText;
   }
-  
+
   for (let i = 0; i < item_prices.length; i++) {
     item_prices[i] = parseInt(item_prices[i].split("$")[1]);
     subtotal += item_prices[i]
   }
-  
+
   subtotal_span[0].innerText = `$${subtotal}.00`
   const tax = (subtotal * (7.25 / 100));
   tax_span[0].innerText = `$${tax.toFixed(2)}`;
+}
+
+
+const manipulatePrices = () => {
+  tax = (subtotal * taxRate);
+  tax_span[0].innerText = `$${tax.toFixed(2)}`;
+  total_span[0].innerText = `$${calcTotal(subtotal, tax)}`
 }
 
 
@@ -20,7 +27,6 @@ const calcTotal = (subtotal, tax) => {
   total = (subtotal + tax + shipping).toFixed(2);
   return total;
 }
-
 // =========== //
 
 
@@ -33,57 +39,47 @@ const qty = document.querySelectorAll("input");
 let cart_remove = document.getElementsByClassName("cart-remove");
 const plus = document.getElementsByClassName("fa-plus");
 const minus = document.getElementsByClassName("fa-minus");
+const allItems = document.getElementsByClassName("item");
 
 const item_prices = [];
 let subtotal = 0;
 let tax;
-const taxRate = 7.25/100;
+const taxRate = 7.25 / 100;
 // ========== //
 
 initializeCart();
 
-// remove item functionality
+// remove item functionality //
 for (let i = 0; i < cart_remove.length; i++) {
   cart_remove[i].addEventListener("click", () => {
     parent = cart_remove[i].parentElement;
     parent.style.display = 'none';
     subtotal -= item_prices[i];
     subtotal_span[0].innerText = `$${subtotal}.00`
-    tax = (subtotal * taxRate);
-    tax_span[0].innerText = `$${tax.toFixed(2)}`;
-    total_span[0].innerText = `$${calcTotal(subtotal, tax)}`
+    manipulatePrices();
   });
 }
+// ========== //
 
-// +/- item functionality
-
-for (let i = 0; i < plus.length; i++) {
+// +/- item functionality // 
+for (let i = 0; i < allItems.length; i++) {
   plus[i].addEventListener("click", () => {
     qty[i].value++;
     subtotal += item_prices[i];
     subtotal_span[0].innerText = `$${subtotal}.00`
     cart_price_span[i].innerText = `$${item_prices[i]*qty[i].value}.00`;
-    tax = (subtotal * taxRate);
-    tax_span[0].innerText = `$${tax.toFixed(2)}`;
-    total_span[0].innerText = `$${calcTotal(subtotal, tax)}`
-  })
-}
+    manipulatePrices();
+  });
 
-
-for (let i = 0; i < minus.length; i++) {
   minus[i].addEventListener("click", () => {
     if (qty[i].value > 0) {
       qty[i].value--;
       subtotal -= item_prices[i];
       subtotal_span[0].innerText = `$${subtotal}.00`
       cart_price_span[i].innerText = `$${item_prices[i]*qty[i].value}.00`;
-      tax = (subtotal * taxRate);
-      tax_span[0].innerText = `$${tax.toFixed(2)}`;
-      total_span[0].innerText = `$${calcTotal(subtotal, tax)}`
+      manipulatePrices();
     }
   });
 }
 
-
-
-
+//==========//
